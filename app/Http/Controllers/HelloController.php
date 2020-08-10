34b9f338b71b53b930be3c3a15a6e1d014ce794a
Roberto Maciel <lmaciel1303@gmail..com>
2020-08-10 14:53:48 -0500

    Creating about and services functions in HelloController

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(){
+    public function about(){
     
         /*view() accept a second parameter, which is the data we're gonna pass to our view.
         If the variable's name is the same as the name in the array, we can use the function 
         compact() as a shortcut, and this will generate the exact same array as we had before*/
         return view('about');
+    }
+
+    public function services(){
+    
+        return view('services');
     }
 }
