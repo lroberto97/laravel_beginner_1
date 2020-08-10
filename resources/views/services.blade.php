5fbc34074bc38e3a4f3e7521ff3ce9aed7f69835
Roberto Maciel <lmaciel1303@gmail..com>
2020-08-10 14:57:18 -0500

    Updating service view

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,3 +3,14 @@
 {{-- section() allows to change the yield we defined in the view we're extending
 with the following text, we use this just for one simple text --}}
 @section('title', 'Services')
+@section('content')
+    <h1>Welcome to Laravel 6 from Services</h1>
+    <p>With some aditional text from about.</p>
+
+    {{-- ul>li{Services $}*3 shortcut emmet --}}
+    <ul>
+        <li>Services 1</li>
+        <li>Services 2</li>
+        <li>Services 3</li>
+    </ul>
+@endsection
