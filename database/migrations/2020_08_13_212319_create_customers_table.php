85795f3a80c3acac040ca365b1c2742edfc3af8f
Roberto Maciel <lmaciel1303@gmail..com>
2020-08-13 16:28:57 -0500

    Add fields to customers migration

--- database/migrations/2020_08_13_212319_create_customers_table.php
+++ database/migrations/2020_08_13_212319_create_customers_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\Table\Table;
 
 class CreateCustomersTable extends Migration
 {
@@ -14,7 +15,10 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
+            //Adding ->nullable() at the end of the fields, makes it optiotal.
             $table->id();
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
